08d33ffb1ab0c2d803c03c444db271755ea7ffac
ruckeran <135586224+ruckeran@users.noreply.github.com>
2025-01-08 17:34:44 +0100

    started iea function for icils
    still have to figure out
    - do I need to compare the national or international data sets?
    - where is the data of the IT guys?

---
+++ R/download_icils_iea.R
@@ -0,0 +1,108 @@
+#' Download and import an empty FDZ file.
+#'
+#' Download and import an empty ICILS data set from the \href{https://www.iqb.hu-berlin.de/fdz/studies/}{FDZ homepage}.
+#'
+#' The function downloads and imports an empty ICILS data set (\code{Leerdatensatz}) from the FDZ homepage.
+#' These data sets contain zero rows.
+#' The data is imported via \code{\link[eatGADS]{import_spss}} as a \code{GADSdat} object.
+#'
+#'@param year Year of the assessment.
+#'@param data_type Type of the data.
+#'
+#'@examples
+#' iea_icils <- download_icils(year = "2018",
+#'                         data_type = "stud_dat")
+#'@export
+download_icils_iea <- function(year = c("2018", "2013"),
+                               data_type = c("stud_dat", "stud_nat_dat", "stud_int_dat",
+                                             "teach_dat", "teach_nat_dat", "teach_int_dat",
+                                             "school_dat", "school_nat_dat", "school_int_dat",
+                                             "it_dat")) {
+  ## input validation
+  year <- match.arg(year)
+  data_type <- match.arg(data_type)
+
+  # URL table for each study, year and data type
+  download_paths <- list(
+    "2013" = list(
+      stud_nat_dat = list(
+        zip_path = "https://www.iea.nl/sites/default/files/data-repository/ICILS/ICILS2013/ICILS2013_IDB_SPSS.zip",
+        dat_subdir = "BSGDEUI1.sav"
+      ),
+      # stud_int_dat = list(
+      #   zip_path = "https://www.iea.nl/sites/default/files/data-repository/ICILS/ICILS2013/ICILS2013_IDB_SPSS.zip",
+      #   dat_subdir = "CY07_MSU_STU_QQQ.sav"
+      # ),
+      teach_nat_dat = list(
+        zip_path = "https://www.iea.nl/sites/default/files/data-repository/ICILS/ICILS2013/ICILS2013_IDB_SPSS.zip",
+        dat_subdir = "BTGDEUI1.sav"
+      ),
+      # teach_int_dat = list(
+      #   zip_path = "https://www.iea.nl/sites/default/files/data-repository/ICILS/ICILS2013/ICILS2013_IDB_SPSS.zip",
+      #   dat_subdir = "CY07_MSU_STU_QQQ.sav"
+      # ),
+      school_nat_dat = list(
+        zip_path = "https://www.iea.nl/sites/default/files/data-repository/ICILS/ICILS2013/ICILS2013_IDB_SPSS.zip",
+        dat_subdir = "BCGDEUI1.sav"
+      # ),
+      # school_int_dat = list(
+      #   zip_path = "https://www.iea.nl/sites/default/files/data-repository/ICILS/ICILS2013/ICILS2013_IDB_SPSS.zip",
+      #   dat_subdir = "CY07_MSU_STU_QQQ.sav"
+      )
+    ),
+    "2018" = list(
+      stud_dat = list(
+        zip_path = "https://www.iea.nl/sites/default/files/data-repository/ICILS/ICILS2018/ICILS2018_IDB_SPSS.zip",
+        dat_subdir = "BSGDEUI2.sav"
+      ),
+      teach_dat = list(
+        zip_path = "https://www.iea.nl/sites/default/files/data-repository/ICILS/ICILS2018/ICILS2018_IDB_SPSS.zip",
+        dat_subdir = "BTGDEUI2.sav"
+      ),
+      school_dat = list(
+        zip_path = "https://www.iea.nl/sites/default/files/data-repository/ICILS/ICILS2018/ICILS2018_IDB_SPSS.zip",
+        dat_subdir = "BTGDEUI1.sav"
+      # ),
+      # it_dat = list(
+      #   zip_path = "https://www.iea.nl/sites/default/files/data-repository/ICILS/ICILS2018/ICILS2018_IDB_SPSS.zip",
+      #   dat_subdir = "CY07_MSU_STU_QQQ.sav"
+      )
+    )
+  )
+
+  # call up URL for specific combination
+  if (year %in% names(download_paths) &&
+      data_type %in% names(download_paths[[year]])) {
+
+    zip_path <- download_paths[[year]][[data_type]]$zip_path
+    data_subdir <- download_paths[[year]][[data_type]]$data_subdir
+  } else {
+    stop("The corresponding download has not been implemented yet.")
+  }
+
+  ## Set up temporary folder
+  temp_folder <- tempdir()
+
+  # Set timeout for downloading large files
+  old_timeout <- getOption("timeout")
+  options(timeout = max(300, old_timeout))
+  on.exit(options(timeout = old_timeout))
+
+  ## Download the zip file
+  zip_file <- file.path(temp_folder, "icils.zip")
+  utils::download.file(url = zip_path, destfile = zip_file)
+
+  ## Unzip the required file from the archive
+  unzip_folder <- file.path(temp_folder, "unzipped_data")
+  zip::unzip(zipfile = zip_file, files = data_subdir, exdir = unzip_folder)
+
+  ## Construct the full path to the extracted data file
+  extracted_file_path <- file.path(unzip_folder, data_subdir)
+
+  ## Import the data using haven and convert to GADSdat
+  haven_dat <- haven::read_sav(extracted_file_path, n_max = 1, user_na = TRUE)
+  GADS <- eatGADS:::new_savDat(haven_dat)
+  eatGADS:::prepare_labels(GADS, checkVarNames = FALSE, labeledStrings = "drop")
+
+  return(GADS)
+}
