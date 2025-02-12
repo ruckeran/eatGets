2844177c3a9befeeab1a64a927e30bfa7b45e4c2
ruckeran <annegret.rucker@iqb.hu-berlin.de>
2025-02-12 17:10:15 +0100

    update timss_iea function
    links inserted

--- R/download_timss_iea.R
+++ R/download_timss_iea.R
@@ -27,39 +27,39 @@ download_timss <- function(year = c("2019", "2015", "2011", "2007"),
   download_paths <- list(
     "2007" = list(
       stud_par_dat = list(
-        zip_path = "https://www.oecd.org/content/dam/oecd/en/data/datasets/pisa/pisa-2000-datasets/data-sets-in-txt-formats/intcogn_v4.zip", dat_subdir = "intcogn_v4.txt"),
+        zip_path = "https://www.iea.nl/sites/default/files/data-repository/TIMSS/TIMSS2007/TIMSS2007_IDB_SPSS_G4.zip", dat_subdir = c("asadeum4.sav", "asgdeum4.sav", "asrdeum4.sav", "astdeum4.sav")),
       teach_dat = list(
-        zip_path = "https://www.oecd.org/content/dam/oecd/en/data/datasets/pisa/pisa-2000-datasets/data-sets-in-txt-formats/intscho.zip", dat_subdir = "intscho.txt"),
+        zip_path = "https://www.iea.nl/sites/default/files/data-repository/TIMSS/TIMSS2007/TIMSS2007_IDB_SPSS_G4.zip", dat_subdir = "atgdeum4.sav"),
       school_dat = list(
-        zip_path = "https://www.oecd.org/content/dam/oecd/en/data/datasets/pisa/pisa-2000-datasets/data-sets-in-txt-formats/intscho.zip", dat_subdir = "intscho.txt")
+        zip_path = "https://www.iea.nl/sites/default/files/data-repository/TIMSS/TIMSS2007/TIMSS2007_IDB_SPSS_G4.zip", dat_subdir = "acgdeum4.sav")
     ),
     "2011" = list(
       stud_par_dat = list(
-        zip_path = "https://www.oecd.org/content/dam/oecd/en/data/datasets/pisa/pisa-2000-datasets/data-sets-in-txt-formats/intcogn_v4.zip", dat_subdir = "intcogn_v4.txt"),
+        zip_path = "https://www.iea.nl/sites/default/files/data-repository/TIMSS/TIMSS2011/TIMSS2011_IDB_SPSS_G4.zip", dat_subdir = c("asadeum5.sav", "asgdeum5.sav", "ashdeum5.sav", "asrdeum5.sav", "astdeum5.sav")),
       teach_dat = list(
-        zip_path = "https://www.oecd.org/content/dam/oecd/en/data/datasets/pisa/pisa-2000-datasets/data-sets-in-txt-formats/intscho.zip", dat_subdir = "intscho.txt"),
+        zip_path = "https://www.iea.nl/sites/default/files/data-repository/TIMSS/TIMSS2011/TIMSS2011_IDB_SPSS_G4.zip", dat_subdir = "atgdeum5.sav"),
       school_dat = list(
-        zip_path = "https://www.oecd.org/content/dam/oecd/en/data/datasets/pisa/pisa-2000-datasets/data-sets-in-txt-formats/intscho.zip", dat_subdir = "intscho.txt")
+        zip_path = "https://www.iea.nl/sites/default/files/data-repository/TIMSS/TIMSS2011/TIMSS2011_IDB_SPSS_G4.zip", dat_subdir = "acgdeum5.sav")
     ),
     "2015" = list(
       stud_par_dat = list(
-        zip_path = "https://www.oecd.org/content/dam/oecd/en/data/datasets/pisa/pisa-2000-datasets/data-sets-in-txt-formats/intcogn_v4.zip", dat_subdir = "intcogn_v4.txt"),
+        zip_path = "https://www.oecd.org/content/dam/oecd/en/data/datasets/pisa/pisa-2000-datasets/data-sets-in-txt-formats/intcogn_v4.zip", dat_subdir = c("ASADEUM6.sav", "ASGDEUM6.sav", "ASHDEUM6.sav", "ASRDEUM6.sav", "ASTDEUM6.sav")),
       teach_dat = list(
-        zip_path = "https://www.oecd.org/content/dam/oecd/en/data/datasets/pisa/pisa-2000-datasets/data-sets-in-txt-formats/intscho.zip", dat_subdir = "intscho.txt"),
+        zip_path = "https://www.oecd.org/content/dam/oecd/en/data/datasets/pisa/pisa-2000-datasets/data-sets-in-txt-formats/intscho.zip", dat_subdir = "ATGDEUM6.sav"),
       school_dat = list(
-        zip_path = "https://www.oecd.org/content/dam/oecd/en/data/datasets/pisa/pisa-2000-datasets/data-sets-in-txt-formats/intscho.zip", dat_subdir = "intscho.txt")
+        zip_path = "https://www.oecd.org/content/dam/oecd/en/data/datasets/pisa/pisa-2000-datasets/data-sets-in-txt-formats/intscho.zip", dat_subdir = "ACGDEUM6.sav")
     ),
     "2019" = list(
       stud_par_dat = list(
-        zip_path = "https://www.oecd.org/content/dam/oecd/en/data/datasets/pisa/pisa-2000-datasets/data-sets-in-txt-formats/intcogn_v4.zip", dat_subdir = "intcogn_v4.txt"),
+        zip_path = "https://www.iea.nl/sites/default/files/data-repository/TIMSS/TIMSS2019/TIMSS2019_IDB_SPSS_G4.zip", dat_subdir = c("asadeub7.sav", "asadeum7.sav", "asadeuz7.sav", "asgdeub7.sav", "asgdeum7.sav", "asgdeuz7.sav", "ashdeub7.sav", "ashdeum7.sav", "ashdeuz7.sav", "asrdeub7.sav", "asrdeum7.sav", "asrdeuz7.sav", "astdeub7.sav", "astdeum7.sav", "astdeuz7.sav")),
       teach_dat = list(
-        zip_path = "https://www.oecd.org/content/dam/oecd/en/data/datasets/pisa/pisa-2000-datasets/data-sets-in-txt-formats/intscho.zip", dat_subdir = "intscho.txt"),
-      teach_stud_dat = list(
-        zip_path = "https://www.oecd.org/content/dam/oecd/en/data/datasets/pisa/pisa-2000-datasets/data-sets-in-txt-formats/intscho.zip", dat_subdir = "intscho.txt"),
+        zip_path = "https://www.iea.nl/sites/default/files/data-repository/TIMSS/TIMSS2019/TIMSS2019_IDB_SPSS_G4.zip", dat_subdir = c("atgdeub7.sav", "atgdeum7.sav", "atgdeuz7.sav")),
+      # teach_stud_dat = list(
+      #   zip_path = "https://www.iea.nl/sites/default/files/data-repository/TIMSS/TIMSS2019/TIMSS2019_IDB_SPSS_G4.zip", dat_subdir = "?"),
       school_dat = list(
-        zip_path = "https://www.oecd.org/content/dam/oecd/en/data/datasets/pisa/pisa-2000-datasets/data-sets-in-txt-formats/intscho.zip", dat_subdir = "intscho.txt"),
-      tracking = list(
-        zip_path = "https://www.oecd.org/content/dam/oecd/en/data/datasets/pisa/pisa-2000-datasets/data-sets-in-txt-formats/intscho.zip", dat_subdir = "intscho.txt")
+        zip_path = "https://www.iea.nl/sites/default/files/data-repository/TIMSS/TIMSS2019/TIMSS2019_IDB_SPSS_G4.zip", dat_subdir = c("acgdeub7.sav", "acgdeum7.sav", "acgdeuz7.sav"))
+      # tracking = list(
+      #   zip_path = "https://www.iea.nl/sites/default/files/data-repository/TIMSS/TIMSS2019/TIMSS2019_IDB_SPSS_G4.zip", dat_subdir = "?")
     )
   )
 
